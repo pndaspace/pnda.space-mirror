63b42d7e58a733ae0596661f25bcb5f81f95f682
pndaspace <pndaspace@noreply.codeberg.org>
2024-09-18 10:43:08 +0100

    Temporarily use pages.dev domain

--- lib/get-site-url.ts
+++ lib/get-site-url.ts
@@ -1,5 +1,5 @@
 export default function getSiteUrl() {
     return process.env.NODE_ENV === 'production'
-        ? 'https://pnda.space'
+        ? 'https://pnda-space-mirror.pages.dev/'
         : 'http://localhost:3000';
 }
