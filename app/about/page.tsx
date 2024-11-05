e09ad99ccd6582e5835160e2ca261583d9cb851f
pndaspace <pnda@pnda.space>
2024-11-05 17:01:01 +0000

    Add fun fact to about page oooooh

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -55,7 +55,8 @@ export default async function About() {
                 <Link href="https://github.com/xero/figlet-fonts/blob/master/Elite.flf">Elite</Link>
                 {' '}Figlet font. I use <Link href="https://draculatheme.com/">Dracula</Link> for my
                 dark theme and <Link href="https://horizontheme.netlify.app/">Horizon</Link> for my
-                light theme. Cursors are from {' '}
+                light theme. (fun fact these affect <Highlight>everything</Highlight> on the site,
+                including the logo and cursor trails! Cursors are from {' '}
                 <Link href="https://otakuworld.com/index.html?/toys/preview/icons/sailormooncursors.html">Otaku World</Link>
                 {' '} and modified (poorly) by me. The cute kitty cat following the cursor was made
                 by <Link href="https://github.com/adryd325/oneko.js">adryd325</Link>, a JS port of
